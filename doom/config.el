734a9889cd1e2c1ffd7e8edd80f2f6798b2e9a41
lenehc <chenel.nguessan-laurent@outlook.com>
2024-08-18 18:33:05 +0200

    yuh

--- doom/config.el
+++ doom/config.el
@@ -1,47 +1,12 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-
 (setq doom-font                 (font-spec :family "MesloLGS Nerd Font" :size 12 :weight 'regular)
       doom-variable-pitch-font  (font-spec :family "Georgia" :size 14 :weight 'medium))
-
 (setq doom-theme 'doom-gruvbox)
+(setq display-line-numbers-type t)
+(setq confirm-kill-emacs nil)
 
-(defun org-settings ()
-  (visual-line-fill-column-mode)
-  (setq visual-fill-column-width 90)
-  (setq visual-fill-column-center-text t)
-  (display-line-numbers-mode -1)
-  )
-
-(add-hook 'org-mode-hook 'org-settings)
+(remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-footer)
 
 (require 'ol)
 (org-link-set-parameters "zotero" :follow
@@ -49,52 +14,49 @@
                            (browse-url
                              (format "zotero:%s" zpath))))
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
+(defun org-settings ()
+  (visual-line-fill-column-mode)
+  (setq visual-fill-column-width 90)
+  (setq visual-fill-column-center-text t)
+  (setq org-startup-indented t)
+  (display-line-numbers-mode -1))
+(add-hook 'org-mode-hook 'org-settings)
+
 (setq org-directory "~/notes/org")
 (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
 (setq org-mobile-inbox-for-pull "~/notes/org/mobileorg/inbox.org")
 (setq org-roam-directory "~/notes/roam/")
-(setq org-startup-indented t)
+(setq org-roam-dailies-directory "journal/")
 
-;; Don't display exit confirmation
-(setq confirm-kill-emacs nil)
+(use-package! org-roam
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n d" . org-id-get-create)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode))
 
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
+(setq org-roam-capture-templates
+      '(("m" "main" plain "%?"
+         :if-new (file+head "${slug}.org"
+                            "#+title: ${title}\n\n")
+         :unnarrowed t
+         :empty-lines 1)
+        ("d" "default" plain "%?"
+         :if-new (file+head "%<%Y-%m-%d %H.%M.%S>-${slug}.org"
+                            "#+title: ${title}\n\n")
+         :unnarrowed t
+         :empty-lines 1)))
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(setq org-roam-dailies-capture-templates
+      '(("d" "default" entry
+         "* %?"
+         :target (file+head "%<%Y>/%<%m>/%<%Y-%m-%d>.org"
+                            "#+title: %<%Y-%m-%d>\n\n")
+         :unnarrowed t
+         :empty-lines 1)))
